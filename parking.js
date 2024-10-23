bc92a12bc2600454986017119925d8081a645aea
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-10-23 13:58:33 -0400

    ebhetghwgh5g

--- parking.js
+++ parking.js
@@ -1,6 +1,6 @@
-const n = 5;
-const oneN = "C...C";
-const twoN = "CC..C";
+const n = 8;
+const oneN = "CCC.C..C";
+const twoN = "CC..C.C.";
 function findSameSpots() {
   let counter = 0;
   if (!Number.isInteger(n) || n < 1 || n > 100) {
@@ -9,8 +9,10 @@ function findSameSpots() {
   arrayOne = oneN.split("");
   arrayTwo = twoN.split("");
   for (i = 0; i <= n; i++) {
-    if (arrayOne[i] === arrayTwo[i]) counter += 1;
+    if ((arrayOne[i] === arrayTwo[i]) & (arrayOne[i] === "C")) {
+      counter++;
+    }
   }
   return `there were ${counter} spaces occupied both days`;
 }
-console.log(findSameSpots);
+console.log(findSameSpots());
