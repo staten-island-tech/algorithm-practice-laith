353617d55699aaf4d8e355d316a5764d61379bcc
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-10-24 13:59:44 -0400

    hiohiohji

--- binarydeci.js
+++ binarydeci.js
@@ -1,1 +1,19 @@
-function deciBin(num) {}
+function valueCheck(num, binary) {
+  if (num === 0) {
+    return binary;
+  }
+}
+
+function deciBin(num) {
+  let binary = "";
+  while (num >= 1) {
+    remainder = num % 2;
+    num = num / 2;
+    if ((remainder = 1)) {
+      binary += 1;
+    } else {
+      binary += 0;
+    }
+  }
+  return binary.toString().reverse();
+}
